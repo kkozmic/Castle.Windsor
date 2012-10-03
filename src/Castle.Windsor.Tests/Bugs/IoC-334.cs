4fb1b349a734ae5b0367129cf4a5bbf7922f1ec1
Krzysztof Kozmic <krzysztof@kozmic.pl>
2012-10-03 18:55:31 +1000

    - removing obsolete API from Windsor/Kernel

--- src/Castle.Windsor.Tests/Bugs/IoC-334.cs
+++ src/Castle.Windsor.Tests/Bugs/IoC-334.cs
@@ -12,7 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+
 #pragma warning disable 618
+
 namespace CastleTests.Bugs
 {
 	using Castle.Core.Configuration;
--- src/Castle.Windsor/Windsor/WindsorServiceProvider.cs
+++ src/Castle.Windsor/Windsor/WindsorServiceProvider.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2004-2011 Castle Project - http://www.castleproject.org/
+﻿// Copyright 2004-2012 Castle Project - http://www.castleproject.org/
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -20,8 +20,7 @@ namespace Castle.Windsor
 	using Castle.MicroKernel;
 
 	/// <summary>
-	///   Implementation of <see cref = "IServiceProvider" /> and <see cref = "IServiceProviderEx" /> that uses a <see
-	///    cref = "IWindsorContainer" /> or <see cref = "IKernel" /> as its component's source.
+	/// Implementation of <see cref = "IServiceProvider" /> and <see cref = "IServiceProviderEx" /> that uses a <see cref = "IWindsorContainer" /> or <see cref = "IKernel" /> as its component's source.
 	/// </summary>
 	public class WindsorServiceProvider : IServiceProviderEx
 	{
